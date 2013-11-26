e081278b3de1269e23d043e9c4854c304187199b
gerbal <gerbalblaste@gmail.com>
2013-11-25 20:36:06 -0500

    prettified

--- background.js
+++ background.js
@@ -1,50 +1,48 @@
-var logging = true;
+var logging = true; //enables logging in the broswers JS console.
 var table = Object;
-
-chrome.runtime.onConnect.addListener(function(port){
-      console.assert(port.name == "uriexchange");
-      if (logging){console.log("connected: " + port.name)};
-      port.onMessage.addListener(function(msg) {
-        var table = generateScoreBox.getStatsPage_(msg.uri);
-        if (logging){console.log("uri: " + msg.uri)};
-        port.postMessage({table: score});
-      });
-        // console.log(sender.tab ?
-        //     "from a content script:" + sender.tab.url :
-        //     "from the extension:" + request);
-        // console.log("request: " + request.uri);
-        // // if (request.greeting == "hello")
-        // //   sendResponse({farewell: "goodbye"});
-        // var uri = request.uri;
-        // if (logging){console.log("uri: " + uri);};
-        // var tables = generateScoreBox.getStatsPage_(uri);
-        // if (logging){console.log("tables: " +tables);};
-        // sendResponse({
-        //     table: tables
-        // });
-    });
-
-if (logging){console.log("table: " + table)};
-var score = generateScoreBox.extractStats_(table);
-
-var generateScoreBox = {
-    getStatsPage_: function (statsuri) {
-      var xml = Object;
-      var xhr = new XMLHttpRequest();
-      xhr.onload = function() {
-        var xml = this.responseXML;
-        if (logging){console.log("xml: " +xml);};
-        var xml = xml.getElementById('generalstats');
-        if (logging){console.log("xml: " +xml);};
-      }
-      xhr.open("GET", statsuri);
-      xhr.responseType = "document";
-      xhr.send();
-      return xml;
-      },
-
-
-    extractStats_: function (e) {
-      return e;
+chrome.runtime.onConnect.addListener(function (port) {
+    //listens for a message from the content.js script
+    console.assert(port.name == "uriexchange");
+    if (logging) {
+        console.log("connected: " + port.name);
     }
+    port.onMessage.addListener(function (msg) { // listens for the URI to get passes
+        var table = generateScoreBox.getStatsPage_(msg.uri);
+        if (logging) {
+            console.log("uri: " + msg.uri);
+        }
+        port.postMessage({
+            table: score
+        }); //sends back a score.
+    });
+});
+if (logging) {
+    console.log("table: " + table);
 }
+var score = generateScoreBox.extractStats_(table); //broken. I don't understand why.
+var generateScoreBox = {
+    //big dictionary object full of methods, one of many ways to do things in javascript.
+    getStatsPage_: function (statsuri) {
+        //Hand the URI from the content.js, pulls it down, does some basic parsing. 
+        var xml = Object;
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            var xml = this.responseXML;
+            if (logging) {
+                console.log("xml: " + xml);
+            }
+            xml = xml.getElementById('generalstats'); //works
+            if (logging) {
+                console.log("xml: " + xml);
+            }
+        }
+        xhr.open("GET", statsuri);
+        xhr.responseType = "document";
+        xhr.send();
+        return xml; //returns an object, not sure what to do with it.
+    },
+    extractStats_: function (e) {
+        // I throw an error, no one knows why.
+        return e;
+    }
+};
